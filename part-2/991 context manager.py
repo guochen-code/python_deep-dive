ee35834a4024171c00f2906a74f1bc69f07c9d22
guochen-code <83231868+guochen-code@users.noreply.github.com>
2022-11-01 23:02:09 -0600

    Create 991 context manager.py

---
+++ part-2/991 context manager.py
@@ -0,0 +1,9 @@
+'''
+very useful for anythin that needs to provide Enter/Exit Start/Stop Set/Reset
+
+-> open / close file
+-> start db transaction / commit or abort transaction
+-> set decimal precision to 3 / reset back to orignal precision
+'''
+
+
