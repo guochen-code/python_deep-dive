b6f30554f383d575dc01192f745fbc690a887e53
guochen-code <83231868+guochen-code@users.noreply.github.com>
2022-11-13 19:31:13 -0700

    Create 992 generators and context managers.py

---
+++ part-2/992 generators and context managers.py
@@ -0,0 +1,6 @@
+please see attached ipynb
+
+there is a very close relationship between context manager and generator function
+
+with statement and context managers were written with that precise goal in mind. They want to be able to use generator functions to easily
+create context manager when they came up with this idea of context manager and how to implement it.
