f74d52c694a6625deaa87ef2b8b0f7a19f95f249
guochen-code <83231868+guochen-code@users.noreply.github.com>
2022-11-13 21:56:30 -0700

    Update 993 context manager decorator.py

--- part-2/993 context manager decorator.py
+++ part-2/993 context manager decorator.py
@@ -4,6 +4,13 @@ using a decorator to encapsulate these steps:
   (1) gen = gen_func(args)
   (2) with GenContextManager(gen):
         ...
+
+def gen(args):
+  # setup happens herem or inside try
+  try:
+    yield obj # whatever normally gets returned by __enter__
+  finally:
+    # perform clean up code here
       
 def contextmanager_dec(gen_fn):
   def helper(*args,**kwargs):
@@ -38,3 +45,74 @@ calling open_file(f_name)
     -> creates the generator object
     -> returns GenContextManager instance
   -> with open_file(f_name)
+
+***************************************************************************************************************************************
+(1) create generator function
+def open_file(name,mode='r'):
+  print('opening file...')
+  f=open(fname,mode)
+  try:
+    yield f
+  finally:
+    print('closing file...')
+    f.close()
+    
+(2) create context manager class
+class GenContextManager:
+  def__init__(self,gen):   # receive generator object. not function because want to create function within the decorator below
+    self.gen=gen
+  def __enter__(self):
+    print('calling next to get the yielded value from generator')
+    return next(self.gen)
+  def __exit__(self,exc_type,exc_value,exc_tb):
+    print('calling next to perform cleanup in generator')
+    try:
+      next(self.gen)
+    exept StopIteration:
+      pass
+    return False
+
+file_gen=open_file('test.txt','w')
+with GenContextManager(file_gen) as f:
+  f.writelines('Sir Spamlot')
+  
+**** create a decorator
+def context_manager_dex(gen_fn): # receive generator function not object
+  def helper(*args,**kwargs):
+    gen=gen_fn(*args,**kwargs)
+    ctx=GenContextManager(gen)
+    return ctx
+  return helper
+
+***********************************
+@context_manager_dec
+def open_file(name,mode='r'):
+  print('opening file...')
+  f=open(fname,mode)
+  try:
+    yield f
+  finally:
+    print('closing file...')
+    f.close()
+*********************************** equivalent to writting this way: open_file=context_manager_dec(open_file)
+  
+# now open_file is our context manager
+with open_file('test.txt') as f:
+  print(f.readlines())
+  
+*********************************** python built-in take care of all above !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+from contextlib import contextmanager
+@contextmanager
+def open_file(name,mode='r'):
+  print('opening file...')
+  f=open(fname,mode)
+  try:
+    yield f
+  finally:
+    print('closing file...')
+    f.close()
+# this works to create a context manager out of a generator function
+
+******************************************************************************************************************************************************
+application-1: timer
+# go back to context manager folder - timer.py
