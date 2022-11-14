b093791044f66808aaf6d10b992c2cdbecf3ee51
guochen-code <83231868+guochen-code@users.noreply.github.com>
2022-11-13 22:13:34 -0700

    Update 03 stdout.py

--- context manger/03 stdout.py
+++ context manger/03 stdout.py
@@ -22,3 +22,32 @@ with OutToFile('test.txt'): # if __enter__ not return anything, we don't need as
   print('Line 1')
   print('Line 2')
   
+***************************************************************************************************** generator + context manager
+import sys
+@contextmanager
+def out_to_file(fname):
+  current_stdout=sys.stdout
+  file=open(fname,'w')
+  sys.stdout=file
+  try:
+    yield None
+  finally:
+    file.close()
+    sys.stdout=current_stdout
+    
+with out_to_file('test.txt'): 
+  print('Line 1')
+  print('Line 2')
+  
+***************************************************************************************************** python built-in
+# our method above only redirect to file, built-in can be more generic than just sending it to a file
+# we can redict it to a file object, not a file
+from contextlib import redirect_stdout
+with open('test.txt','w') as f:
+  with redirect_stdout(f):
+    print('look on')
+
+with open('test.txt','r') as f:
+  print(f.readlines())
+
+
