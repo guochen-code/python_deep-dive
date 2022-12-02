0e117981e8363e60d4857f26e4c8489e6c4032a5
guochen-code <83231868+guochen-code@users.noreply.github.com>
2022-12-02 16:27:05 -0700

    Update 019 zipping.py

--- part-2/019 zipping.py
+++ part-2/019 zipping.py
@@ -13,3 +13,18 @@ a=zip(l1,l2,l3)
 iter(a) is a -> True
 # if iterator
 '__next__' in dir(a) -> True
+
+**************************************** be aware
+infinite iterator
+
+iter1 = infinite
+iter2 = infinite
+
+cannot zip longest, because they are infinite
+but it works if we give it a finite iterable
+
+list(zip(range(10,iter1,iter2)) # this is ok
+
+list(zip_longest(range(10,iter1,iter2)) # will cause a problem
+     
+     
