e811d55992bf29d4777e742038921eac7beab529
guochen-code <83231868+guochen-code@users.noreply.github.com>
2022-12-05 22:10:36 -0700

    Update 021 delegate to parent.py

--- part-4/021 delegate to parent.py
+++ part-4/021 delegate to parent.py
@@ -15,8 +15,29 @@ class Student(Pserson):
 
 class Student:
   def __init__(self,name,age,major):
-    super.().__init__(name,age)
+    super().__init__(name,age)
     self.major=major # encouraged to do this way, if reverse these two lines, parent may override child if have sth in common
+    
+******************************************************************************************************************************************************
+
+class Person:
+  def work(self):
+    return 'Person works...'
+  
+class Student(Person):
+  def work(self):
+    result = super().work() # inheritage all the way up, if not in parent, will look into grandparent, until find its first one otherwise exception
+    return result
+  
+------ the problem above is due to name work, if you use a different name, you do can this:
+class Student(Person):
+  def code(self):
+    result = self.work() # because no work here, it will look into the parent class and upper.....
+# this work method will be bound to an instance of Student, not Person......
+
+
+    
+
     
 
     
